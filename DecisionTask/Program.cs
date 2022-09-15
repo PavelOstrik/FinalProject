f01da2db8f036e3c08b3e06bdfb17517c015d4d6
Postrik <postrik@list.ru>
2022-09-15 14:04:58 +0500

    add Change Decision Task For Wrong

--- DecisionTask/Program.cs
+++ DecisionTask/Program.cs
@@ -1,4 +1,6 @@
-﻿string[] array = {"bird", "cow", "cat", "dog", "fish", "mouse"};
+﻿string[] array = { "bird", "cow", "cat", "dog", "fish", "mouse" };
+
+string[] newArray = {"", "", "", "", "", ""};
 
 void PrintArray(string[] matrix)
 {
@@ -6,24 +8,28 @@ void PrintArray(string[] matrix)
     Console.Write("[\t");
     for (int i = 0; i < length; i++)
     {
+        if (matrix[i] != string.Empty)
         Console.Write("'" + matrix[i] + "'" + ",\t");
     }
     Console.WriteLine("]");
 }
 
-void SelectionArray(string[] matrix, int countChar)
+void SelectionArray(string[] matrix, string[] newMatrix, int countChar)
 {
+    int j = 0;      
     int length = matrix.Length;
-    Console.Write("[\t");
     for (int i = 0; i < length; i++)
-    {        
-        if (matrix[i].Length <= countChar) 
+    {
+        if (matrix[i].Length <= countChar)
         {
-            Console.Write("'" + matrix[i] + "'" + ",\t");
+            newMatrix[j] = matrix[i];
+            j++;            
         }
     }
-    Console.WriteLine("]");
 }
 
+
 PrintArray(array);
-SelectionArray(array,3);
+SelectionArray(array,newArray, 3);
+PrintArray(newArray);
+
