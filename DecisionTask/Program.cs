b4f4ca30b3b3f63d4d7805e3632f5a9888f9ad00
Postrik <postrik@list.ru>
2022-09-15 12:42:38 +0500

    add Decision Task

---
+++ DecisionTask/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array = {"bird", "cow", "cat", "dog", "fish", "mouse"};
+
+void PrintArray(string[] matrix)
+{
+    int length = matrix.Length;
+    Console.Write("[\t");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write("'" + matrix[i] + "'" + ",\t");
+    }
+    Console.WriteLine("]");
+}
+
+void SelectionArray(string[] matrix, int countChar)
+{
+    int length = matrix.Length;
+    Console.Write("[\t");
+    for (int i = 0; i < length; i++)
+    {        
+        if (matrix[i].Length <= countChar) 
+        {
+            Console.Write("'" + matrix[i] + "'" + ",\t");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+PrintArray(array);
+SelectionArray(array,3);
